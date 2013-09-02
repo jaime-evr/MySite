74e555f9da5165a9b837269165df20b57679637b
jaime <jaime.victoria.90@gmail.com>
2013-09-01 19:47:19 -0500

    initialized project and configuration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JaimeVictoriaSite::Application.config.secret_key_base = 'ab6660a04452a44d59d6a8a91d665db284ff0f475f0413b242404a7c32c95043e2b5c350b180b7aaf39e0f81075c15a1f2986a5f0bdf8ce0989f13b41ae9d345'
